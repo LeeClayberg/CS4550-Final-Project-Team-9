2cceb358af77921b79fee12c4e9fc00e3f9bec21
Lee Clayberg <lee.clayberg@gmail.com>
2020-05-27 15:17:22 -0400

    Small field changes

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SearchService from "../services/SearchService";
+import ReactHtmlParser from 'react-html-parser';
 
 class Issue extends React.Component {
     state = {
@@ -15,6 +16,12 @@ class Issue extends React.Component {
                 })})
     }
 
+    displayList = (character, i) => {
+        return character.name +
+            ((i < this.state.issue.character_credits.length - 1) ?
+            ", " : "")
+    }
+
     render() {
         if(!this.state.issue) {
             return (<div/>)
@@ -25,7 +32,7 @@ class Issue extends React.Component {
                     <div className="wbdv-issue-info">
                         <span className="row">
                             <div className="col-5">
-                                <img className="wbdv-issue-cover" src={"https://comicvine1.cbsistatic.com/uploads/scale_large/11/117763/2403520-ss16.png"}
+                                <img className="wbdv-issue-cover" src={this.state.issue.image.super_url}
                                      alt="Card image cap"/>
                             </div>
                             <span className="col-7 wbdv-basic-top-outer">
@@ -40,53 +47,57 @@ class Issue extends React.Component {
                                         <div className="col-3 wbdv-info-row-column font-weight-bold">
                                             Name:
                                         </div>
-                                        <div className="col-9 wbdv-info-row-column text-right">
-                                            In the Hands of ... Mephisto!
+                                        <div className="col-9 wbdv-info-row-column text-right text-truncate">
+                                            {this.state.issue.name?
+                                             this.state.issue.name :
+                                             this.state.issue.volume.name + " #" + this.state.issue.issue_number}
                                         </div>
                                     </div>
                                     <div className="row wbdv-info-row">
                                         <div className="col-4 wbdv-info-row-column font-weight-bold">
                                             Volume:
                                         </div>
-                                        <div className="col-8 wbdv-info-row-column text-right">
-                                            The Silver Surfer
+                                        <div className="col-8 wbdv-info-row-column text-right text-truncate">
+                                            {this.state.issue.volume.name}
                                         </div>
                                     </div>
                                     <div className="row wbdv-info-row">
                                         <div className="col-4 wbdv-info-row-column font-weight-bold">
                                             Issue:
                                         </div>
-                                        <div className="col-8 wbdv-info-row-column text-right">
-                                            16
+                                        <div className="col-8 wbdv-info-row-column text-right text-truncate">
+                                            {this.state.issue.issue_number}
                                         </div>
                                     </div>
                                     <div className="row wbdv-info-row">
                                         <div className="col-4 wbdv-info-row-column font-weight-bold">
                                             Cover Date:
                                         </div>
-                                        <div className="col-8 wbdv-info-row-column text-right">
-                                            May 1970
+                                        <div className="col-8 wbdv-info-row-column text-right text-truncate">
+                                            {this.state.issue.cover_date}
                                         </div>
                                     </div>
                                     <div className="row wbdv-info-row">
                                         <div className="col-4 wbdv-info-row-column font-weight-bold">
                                             In Store Date:
                                         </div>
-                                        <div className="col-8 wbdv-info-row-column text-right">
-                                             N/A
+                                        <div className="col-8 wbdv-info-row-column text-right text-truncate">
+                                             {this.state.issue.store_date?
+                                              this.state.issue.store_date :
+                                              "N/A"}
                                         </div>
                                     </div>
                                 </div>
                                 <div className="wbdv-top-spacer"/>
                                 <div className="wbdv-top-chars">
                                     <div className="font-weight-bold wbdv-section-title">Characters</div>
-                                     Dum Dum Dugan, Mephisto, Nick Fury, Shalla Bal, Silver Surfer
+                                    {this.state.issue.character_credits.map(this.displayList)}
                                 </div>
                             </span>
                         </span>
                         <div className="wbdv-issue-description">
                             <div className="font-weight-bold wbdv-section-title">Description</div>
-                            <p><em>Will Mephisto finally make an offer Silver Surfer can’t refuse?!</em></p><p>Mephisto has yet another plan to obtain the Silver Surfer's soul. He disguises himself and walks among the humans. He now comes up with another plan.</p><p>He transports the Surfer to him. This time he claims to be a friend to the Surfer. He allows the Surfer to pass through the barrier and return home to Zenn-La. But when the Surfer arrives, Shalla Bal is missing, taken by Mephisto himself.</p><p>He returns back to Earth and is trapped once again. Mephisto revels in his frustration. They battle but the Surfer is no match for him. Above the planet they create giant surges of energy. This attracts the attention of SHIELD.</p><p>Mephisto finally reveals his plan to the Surfer and shows him the image of Shalla Bal, lost among billions on Earth, freezing and starving. If the Surfer pledges himself to Mephisto he will reunite them. The Surfer gives in and as a test Mephisto tells him that he must destroy SHIELD.</p>
+                            {ReactHtmlParser(this.state.issue.description)}
                         </div>
                         <div className="wbdv-issue-related-header">
                                 Related Issues
