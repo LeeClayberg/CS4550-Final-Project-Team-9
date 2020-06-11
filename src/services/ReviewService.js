85d1b7d8a067253016238de90a10733011c2fb8c
Lee Clayberg <lee.clayberg@gmail.com>
2020-06-10 23:08:40 -0400

    Review service

---
+++ src/services/ReviewService.js
@@ -0,0 +1,48 @@
+
+const server = "https://comicstack.herokuapp.com/api";
+
+const createReview = (review) =>
+    fetch(server + "/reviews", {
+        method: 'POST',
+        body: JSON.stringify(review),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+
+const deleteReview = (reviewId) =>
+    fetch(server + "/reviews/" + reviewId, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
+
+const findAllReviews = () =>
+    fetch(server + "/reviews")
+        .then(response => response.json())
+
+const findReviewById = (reviewId) =>
+    fetch(server + "/reviews/" + reviewId)
+        .then(response => response.json())
+
+const findReviewsForUser = (userId) =>
+    fetch(server + "/users/" + userId + "/reviews")
+        .then(response => response.json())
+
+const findReviewsForIssue = (issueId) =>
+    fetch(server + "/issues/" + issueId + "/reviews")
+        .then(response => response.json())
+
+const findRecentReviews = () =>
+    fetch(server + "/reviews/recent")
+        .then(response => response.json())
+
+export default {
+    createReview,
+    deleteReview,
+    findAllReviews,
+    findReviewById,
+    findReviewsForUser,
+    findReviewsForIssue,
+    findRecentReviews
+}
